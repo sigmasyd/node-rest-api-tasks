42d258f2c2c32bf11b3ce5c7e6fd941b33021132
Carlos Barcos <fbarcos8@hotmail.com>
2023-03-12 02:28:49 -0500

    Configurando server basico

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import express from 'express'
 
 const app = express()
 
-const PORT = 3000
+app.set('port', process.env.PORT || 3000)
 
-app.listen(PORT)
+app.listen(app.get('port'))
 
-console.log(`Server on port ${PORT}`)
+console.log(`Server on port `, app.get('port'))
