d70cc0ec18fe48025a868240cc56b8e65f3051da
Carlos Barcos <fbarcos8@hotmail.com>
2023-03-13 20:30:50 -0500

    instalando morgan y refactor

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import express from 'express'
+import morgan from 'morgan'
 import TasksRoutes from './routes/tasks.routes'
 
 const app = express()
 
+// settings
 app.set('port', process.env.PORT || 3000)
 
+// middlewares
+app.use(morgan('dev'))
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))  // aceptar form
 
+// routes
 app.get('/', (req, res) => {
   res.json({message: 'Welcome'})
 })
