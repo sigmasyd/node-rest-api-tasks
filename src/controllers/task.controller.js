6c7d73e187a74acbd88034690cdbc0f38a74b764
Carlos Barcos <fbarcos8@hotmail.com>
2023-03-14 05:08:21 -0500

    Agregando try catch y validaciones

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,18 +1,35 @@
 import Task from '../models/Task'
 
 export const createTask = async (req, res) => {
-  const newTask = new Task({
-    title: req.body.title, 
-    description: req.body.description,
-    done: req.body.done ? req.body.done : false
-  })
-  const taskSaved = await newTask.save()
-  res.json(taskSaved)
+  if (!req.body.title) {
+    return res.status(400).send({
+      message: "Title cannot be empty"
+    })
+  }
+  try {
+    const newTask = new Task({
+      title: req.body.title, 
+      description: req.body.description,
+      done: req.body.done ? req.body.done : false
+    })
+    const taskSaved = await newTask.save()
+    res.json(taskSaved)
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || 'Something goes wrong creating a task'
+    })
+  }
 }
 
 export const findAllTasks = async (req, res) => {
-  const tasks = await Task.find()
-  res.json(tasks)
+  try {
+    const tasks = await Task.find()
+    res.json(tasks)
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || 'Something goes wrong retrieving the tasks'
+    })
+  }
 }
 
 export const findAllDoneTask = async (req, res) => {
