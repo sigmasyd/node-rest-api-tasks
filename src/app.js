9551b1a36fda8593919eb6303deb7ba39c2ec3b1
Carlos Barcos <fbarcos8@hotmail.com>
2023-03-13 20:35:16 -0500

    Configurando cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
+
 import TasksRoutes from './routes/tasks.routes'
 
 const app = express()
@@ -8,6 +10,10 @@ const app = express()
 app.set('port', process.env.PORT || 3000)
 
 // middlewares
+const corsOptions = {
+  //origin: 'http://localhost:3000' // si es vacio, acepta peticiones de todos lados
+}
+app.use(cors(corsOptions))
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))  // aceptar form
